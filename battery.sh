f35378c23afab2c6960fa06ced2a99e712b47cfd
Cephi <cephi_sui@protonmail.com>
2024-01-27 17:46:28 -0500

    Add files via upload

--- battery.sh
+++ battery.sh
@@ -3,8 +3,8 @@ if [ $# -eq 0 ]; then echo "Missing battery argument!"; exit 1; fi
 
 # Initial data collection.
 upower -i /org/freedesktop/UPower/devices/battery_$1 | 
-awk '/state/ {printf "{\"alt\": " "\"" $2 "\", "}
-             {printf "{\"class\": " "\"" $2 "\", "} 
+awk '/state/ {printf "{\"alt\": " "\"" $2 "\", ";
+              printf "\"class\": " "\"" $2 "\", "} 
      /time to/ {printf "\"tooltip\": " "\"" $4 " " $5 "\", "} 
      /percentage/ {gsub("%","",$2);
                    printf "\"percentage\": " $2 ", ";
@@ -13,8 +13,8 @@ awk '/state/ {printf "{\"alt\": " "\"" $2 "\", "}
 
 # Listen for battery events and output JSON.
 upower -i /org/freedesktop/UPower/devices/battery_$1 --monitor-detail | 
-awk '/state/ {printf "{\"alt\": " "\"" $2 "\", "}
-             {printf "{\"class\": " "\"" $2 "\", "} 
+awk '/state/ {printf "{\"alt\": " "\"" $2 "\", ";
+              printf "\"class\": " "\"" $2 "\", "} 
      /time to/ {printf "\"tooltip\": " "\"" $4 " " $5 "\", "} 
      /percentage/ {gsub("%","",$2);
                    printf "\"percentage\": " $2 ", ";
